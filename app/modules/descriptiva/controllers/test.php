0266f0c0357731f89abce60762d3ac4ef9bf9899
jesusesmipastor <jesusesmipastor@jhon>
2015-08-20 16:32:17 -0500

    segunto test a la funcion media

--- app/modules/descriptiva/controllers/test.php
+++ app/modules/descriptiva/controllers/test.php
@@ -9,6 +9,8 @@ class Test extends Controller {
 	
    public function index(){
    $this->unit->run( $this->descriptiva_model->media( Array( Array(1,2,3), Array(4,5,1,2) )  ), Array(2,3), "test de la media"  );   
+   $this->unit->run( $this->descriptiva_model->media( Array( Array(1,2), Array(1,4) )  ), Array(1.5,2.5), "test de la media"  );   
+      
    echo $this->unit->report();
    }
 	      
